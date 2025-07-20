2e0e986e1765ff73455018b5b0452af29a66389c
Jens Valdez <125117369+code-a-game@users.noreply.github.com>
2025-07-20 20:09:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,25 +24,25 @@ function switchLanguage() {
 }
 
 function initializeLanguage() {
-    const preferredLanguage = localStorage.getItem('preferredLanguage') || 'da';
+    const preferredLanguage = localStorage.getItem('preferredLanguage') || 'en';
     const danishContent = document.getElementById('danish-content');
     const englishContent = document.getElementById('english-content');
     const languageFlag = document.getElementById('languageFlag');
 
-    if (preferredLanguage === 'en') {
-        // Set to English
-        danishContent.classList.add('hidden');
-        englishContent.classList.remove('hidden');
-        languageFlag.src = 'images/languageIconDK.png';
-        languageFlag.alt = 'Switch to Danish';
-        document.documentElement.lang = 'en';
-    } else {
-        // Set to Danish (default)
+    if (preferredLanguage === 'da') {
+        // Set to Danish
         englishContent.classList.add('hidden');
         danishContent.classList.remove('hidden');
         languageFlag.src = 'images/languageIconUK.png';
         languageFlag.alt = 'Switch to English';
         document.documentElement.lang = 'da';
+    } else {
+        // Set to English (default)
+        danishContent.classList.add('hidden');
+        englishContent.classList.remove('hidden');
+        languageFlag.src = 'images/languageIconDK.png';
+        languageFlag.alt = 'Switch to Danish';
+        document.documentElement.lang = 'en';
     }
 }
 
